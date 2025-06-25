4bb878dc5782cf43709337eceb7a968c8ec8717a
WodaMineralna <raknieborak65@gmail.com>
2025-06-25 21:17:26 +0200

    Using React Query & React Router
    - Added a loader() and action() functions
    - Handling the loading states with react-router hooks
    - Added a global progress bar indicator with useIsFetching Hook

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,46 +1,56 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery, useMutation } from "@tanstack/react-query";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useNavigation,
+  useParams,
+  useSubmit,
+} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { fetchEvent, updateEvent } from "../../util/http.js";
 import { queryClient } from "../../util/queryClient.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
   const { id } = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  // * zostawiamy useQuery() tutaj pomimo uzywania go w loaderze, poniewaz i tak to wszystko bedzie cached
+  // * i zachowujemy nasze advantages z korzystania z useQuery() - np re-fetch gdy user wytabuje poza strone, etc...
+  const { data, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
+    staleTime: 10000,
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (dataPassedToMutate) => {
-      const updatedEventData = dataPassedToMutate.event;
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (dataPassedToMutate) => {
+  //     const updatedEventData = dataPassedToMutate.event;
 
-      await queryClient.cancelQueries({ queryKey: ["events", id] }); // cancels Queries triggered with useQuery()
-      const prevEventData = queryClient.getQueryData(["events", id]);
+  //     await queryClient.cancelQueries({ queryKey: ["events", id] }); // cancels Queries triggered with useQuery()
+  //     const prevEventData = queryClient.getQueryData(["events", id]);
 
-      queryClient.setQueryData(["events", id], updatedEventData);
+  //     queryClient.setQueryData(["events", id], updatedEventData);
 
-      return { prevEventData };
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(["events", id], context.prevEventData);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["events", id]);
-    },
-    // ^ not handling 'isPending' / ... here, because of Optimistic Updating
-  });
+  //     return { prevEventData };
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(["events", id], context.prevEventData);
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(["events", id]);
+  //   },
+  //   // ^ not handling 'isPending' / ... here, because of Optimistic Updating
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id, event: formData });
-    handleClose(); // doing it here, instead of in 'onSuccess()`, because of Optimistic Updating
+    submit(formData, { method: "PUT" }); // ! submit() triggeruje client-side action() Fn, NIE WYSYLA HTTP REQUEST'u!!
   }
 
   function handleClose() {
@@ -49,14 +59,7 @@ export default function EditEvent() {
 
   let content;
 
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
-
+  // ^ moglibysmy uzyc Error Handling capabilities React-Router'a, ale juz ciul w to
   if (isError) {
     content = (
       <>
@@ -76,15 +79,39 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <span>Sending data...</span>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  const id = params.id;
+
+  return queryClient.fetchQuery({
+    queryKey: ["events", id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(["events"]); // papa dla optimistic update'owania - musielibysmy napisac nasza wlasna logike w 'handleSubmit()'
+  return redirect("../");
 }
